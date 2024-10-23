37e78b72385dc4a8a5b99e1d71d26fe9fb4b70b9
Subhitcha.S <115633804+Subhitcha04@users.noreply.github.com>
2024-10-23 18:46:26 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom'; // Import HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter> {/* Use HashRouter instead of BrowserRouter */}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
